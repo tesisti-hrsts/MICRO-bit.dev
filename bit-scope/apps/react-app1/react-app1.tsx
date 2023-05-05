06e11b19d82285fc3d5bf41494f7f7802a557ab7
Paolo Patrone <paolo.patrone99@gmail.com>
2023-05-05 14:30:06 +0200

    Communication between apps done, demo app with Bit Framework completed

--- bit-scope/apps/react-app1/react-app1.tsx
+++ bit-scope/apps/react-app1/react-app1.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Observable } from 'windowed-observable';
 import logoReact from "./assets/react.png";
 import "./styles/react-app1-style.css";
 
@@ -6,8 +7,30 @@ export default function ReactApp1() {
 	const [clicked, setClicked] = useState(false);
 	const handleClick = () => setClicked(!clicked);
 
+	const observable = new Observable('countObservable');
+
+	let count = 0;
+
+	const handleButtonPlus = (e) => {
+		e.preventDefault();
+		count += 1;
+		observable.publish(count);
+	}
+
+	const handleButtonMinus = (e) => {
+		e.preventDefault();
+		if (count > 0) {
+			count -= 1;
+			observable.publish(count);
+		}
+	}
+
 	return (
 		<div id='back_app1'>
+			<div id='divPlusMinus_app1'>
+				<button type='button' id='buttonPlus_app1' onClick={handleButtonPlus}> + </button>
+				<button type='button' id='buttonMinus_app1' onClick={handleButtonMinus}> - </button>
+			</div>
 			<button type='button' id='button_app1' onClick={handleClick}> Click me! </button>
 			<div id='imageDiv_app1'> {clicked ? <img id='img_app1' src={logoReact} alt="React Logo" /> : ""} </div>
 		</div>
--- bit-scope/apps/react-app2/react-app2.tsx
+++ bit-scope/apps/react-app2/react-app2.tsx
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Observable } from 'windowed-observable';
 import logoReact from "./assets/react.png";
 import "./styles/react-app2-style.css";
 
 export default function ReactApp2() {
 	const [clicked, setClicked] = useState(false);
 	const handleClick = () => setClicked(!clicked);
+	const [currCount, setCurrCount] = useState(0);
+
+	const observable = new Observable('countObservable');
+	const handleCurrCount = (remoteCount) => setCurrCount(remoteCount);
+
+	useEffect(() => {
+		observable.subscribe(handleCurrCount);
+
+		return () => {
+			observable.unsubscribe(handleCurrCount)
+		}
+	}, [handleCurrCount]);
 
 	return (
 		<div id='back_app2'>
+			<span id='countPart_app2'> Current count: {currCount} </span>
 			<button type='button' id='button_app2' onClick={handleClick}> Click me! </button>
 			<div id='imageDiv_app2'> {clicked ? <img id='img_app2' src={logoReact} alt="React Logo" /> : ""} </div>
 		</div>
